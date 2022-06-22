91ccbec20a8a8a983cf2515dd9ecfe9e96e07881
Marion <105941364+MarionBrdr@users.noreply.github.com>
2022-06-22 15:16:26 +0200

    Update QCMChrono.php

--- QCMChrono.php
+++ QCMChrono.php
@@ -14,13 +14,17 @@
     <link rel="stylesheet" href="https://chartresxchoeur.web-edu.fr/css/qcm.css"/>
 	<link rel="stylesheet" href="https://chartresxchoeur.web-edu.fr/css/style.css"/>
 </head>
-<?php $id = $_GET["ID"]; ?>
+	
+<?php $id = $_GET["ID"]; ?> <!-- Le script de la page QCMFormChrono.html ajoute en paramètre l'ID en cours d'utilisation lors la réponse au formulaire. Récupération et stockage de la valeur. Cette manipulation justifie le format php de cette page.-->
+	
 <script>
-	var ID = <?php echo $id?>;
+	var ID = <?php echo $id?>; // Récupération de la variable php précédement stockée.
+	var questionnaire = "c"; // Cette variable permet de distinguer la liste de questions des deux QCM dans le script qcm.js.
 </script>
+	
 <body>
     <header class="container-fluid">
-        <nav class="container navbar navbar-expand-lg navbar-light bg-white border-bottom"> <!--fixed-top-->
+        <nav class="container navbar navbar-expand-lg navbar-light bg-white border-bottom">
             <div class="container px-2 py-1">
                 <a class="navbar-brand" id="titre" href="index.html">
                     <img src="/img/logo2.png" alt="" height="90" id="logonavbar">
@@ -30,7 +34,7 @@
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarCollapse">
-                    <ul class="navbar-nav" id="navbarauto"> <!--me-auto mb-2 mb-lg-0-->
+                    <ul class="navbar-nav" id="navbarauto">
                         <li class="nav-item">
                             <a class="nav-link active" id="txtcustom" aria-current="page" href="index.html">Accueil</a>
                         </li>
@@ -40,8 +44,8 @@
                             Quiz
                             </a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="QCMFormChrono.html">Chronologiques</a></li>
-                                <li><a class="dropdown-item" href="QCMFormThema.html">Thématiques</a></li>
+                                <li><a class="dropdown-item" href="QCMFormChrono.html">Chronologique</a></li>
+                                <li><a class="dropdown-item" href="QCMFormThema.html">Thématique</a></li>
                             </ul>
                         </li>
 
@@ -58,7 +62,7 @@
 
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Livrables
+                                Développements numériques
                             </a>
                             <ul class="dropdown-menu container" aria-labelledby="navbarDropdown">
                                 <li><a class="dropdown-item" href="livrables.html">Présentation</a></li>
@@ -71,8 +75,7 @@
                 </div>
             </div>
         </nav>
-    </header>
-	
+    </header>	
 	<section>
 		<div class="container">
 			
@@ -80,37 +83,30 @@
 				<p class="fs-3" class="fw-bolder float-start" id="question">.</p>
 			</div>
 			
-			<div id="game">
-
+			<div id="game"> <!-- Division permettant l'affichage de l'écran de fin, en rendant absent son contenu.-->
 				<div class="text-center">
 					<h4 class="ms-4"><strong>Score :</strong><p id="score">0</p></h4>
 				</div>
-
-				<div class="row justify-content-evenly">
-
-					<img src="js/img/QCMchoeur.jpg" id="img" max-width="40%" height="auto" class="col"/>
-
-					<div id="reponses" class="row col">
-						<div id="reponse1" class="p-2 bg-light border text-center col-4 col-sm-7" type="rep">
+				<div class="row justify-content-center">
+					<div class="col-md-4 col-sm-4">
+						<img src="js/img/N1.png" id="img"/>
+					</div>					
+					<div id="reponses" class="grid col-md-4 col-sm-4">							
+						<div id="reponse1" type="rep" class="g-col-6">
 							<button type="button" data-bs-toggle="modal" id="Bouton1" data-bs-target="#exampleModal"
-									class="btn btn-custom text-white"></button>
+									class="btn btn-custom text-white shadow"></button>
 						</div>
-
-						<div id="reponse2" class="p-2 bg-light border text-center col-4 col-sm-7" type="rep">
+						<div id="reponse2" type="rep" class="g-col-6">
 							<button type="button" data-bs-toggle="modal" id="Bouton2" data-bs-target="#exampleModal"
-									class="btn btn-custom text-white"></button>
+									class="btn btn-custom text-white shadow"></button>
 						</div>
-
-						<div class="w-200 d-none d-md-block"></div>
-
-						<div id="reponse3" class="p-2 bg-light border text-center col-4 col-sm-7" type="rep">
+						<div id="reponse3" type="rep" class="g-col-6">
 							<button type="button" data-bs-toggle="modal" id="Bouton3" data-bs-target="#exampleModal"
-									class="btn btn-custom text-white"></button>
+									class="btn btn-custom text-white shadow"></button>
 						</div>
-
-						<div id="reponse4" class="p-2 bg-light border text-center col-4 col-sm-7" type="rep">
+						<div id="reponse4" type="rep" class="g-col-6">
 							<button type="button" data-bs-toggle="modal" id="Bouton4" data-bs-target="#exampleModal"
-									class="btn btn-custom text-white"></button>
+									class="btn btn-custom text-white shadow"></button>
 						</div>
 					</div>
 
@@ -130,28 +126,22 @@
 							</div>
 						</div>
 					</div>
-				</div>	
-
-				<footer>
-					<div class="container py-3 my-4">
-						<ul class="nav justify-content-center border-top pb-3 mb-3">
-							<li class="nav-item"><a href="mentions.html" class="nav-link px-2 text-muted">Mentions légales</a></li>
-							<li class="nav-item"><a href="contact.html" class="nav-link px-2 text-muted">Contact</a></li>
-						</ul>
-					</div>
-				</footer>
-
+				</div>
 				<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
 						integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-						crossorigin="anonymous"></script>
-				<script src="https://chartresxchoeur.web-edu.fr/js/qcm.js"></script>
-
-				</div>
+						crossorigin="anonymous"></script> <!-- Script des objets bootstrap (ici le modal)-->
+				<script src="https://chartresxchoeur.web-edu.fr/js/qcm.js"></script> <!-- Script du déroulement du QCM-->
 			</div>
-		
-			<!-- <div id="end" visibility="hidden" display="none">
-				<p>Merci d'avoir joué à ce QCM, n'hésitez pas à visiter les pages de présentation des livrables du projet ChArtRes pour approfondir vos connaissances sur la cathédrale !</p>
-			</div> -->
-		</section>
-	</body>
+			
+			<footer>
+				<div class="container py-3 my-4">
+					<ul class="nav justify-content-center border-top pb-3 mb-3">
+						<li class="nav-item"><a href="mentions.html" class="nav-link px-2 text-muted">Mentions légales</a></li>
+						<li class="nav-item"><a href="contact.html" class="nav-link px-2 text-muted">Équipe</a></li>
+					</ul>
+				</div>
+			</footer>
+		</div>
+	</section>
+</body>
 </html>
